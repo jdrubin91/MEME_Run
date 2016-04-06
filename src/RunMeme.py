c6356ca53946e8675f2dcae99ae5879f4590af6a
jdrubin91 <jd.rubin91@gmail.com>
2016-04-06 11:47:02 -0600

    updates

--- src/RunMeme.py
+++ src/RunMeme.py
@@ -6,5 +6,5 @@ import os
 
 def run(fastafile,fastafile1):
     #os.system("meme " + fastafile + " -maxsize 10000000 -dna")
-    os.system("dreme -dna -p " + fastafile + " -n " + fastafile1)
+    os.system("dreme -p " + fastafile + " -n " + fastafile1)
     
