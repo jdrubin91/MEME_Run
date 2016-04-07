cc161aeee7d822e0473f7c3a6af73701d6a3b2da
jdrubin91 <jd.rubin91@gmail.com>
2016-04-07 15:53:15 -0600

    updates

--- src/RunMeme.py
+++ src/RunMeme.py
@@ -5,6 +5,6 @@ __author__ = 'Jonathan Rubin'
 import os
 
 def run(fastafile,fastafile1):
-    #os.system("meme " + fastafile + " -maxsize 10000000 -dna")
-    os.system("dreme -p " + fastafile + " -n " + fastafile1)
+    os.system("meme " + fastafile + " -maxsize 10000000 -dna")
+    #os.system("dreme -p " + fastafile + " -n " + fastafile1)
     
